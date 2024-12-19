04a918ceb6c3a8fb97aabc12139b6d0222731a58
Sarfraz-Ahmad-it <sarfrazahmadiit@gmail.com>
2024-12-19 23:05:58 +0530

    #add java code

---
+++ java.js
@@ -0,0 +1,20 @@
+let display = document.getElementById('inputBox');
+let button = document.querySelectorAll('button');
+buttonArray = Array.from(button);
+let string = '';
+
+buttonArray.forEach(function(btn) {
+    
+    btn.addEventListener('click', function(event){
+
+        if(event.target.innerHTML == 'Del'){
+            string = string.substring(0, string.length-1);
+            display.value = string;
+
+        }else{
+            string += event.target.innerHTML;
+            display.value = string;
+        }
+
+    });
+});
